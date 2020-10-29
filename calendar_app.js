8e682f8df7e8d321297fb56ffad6291a19227136
Liz <lizlarkin@highlandtechnology.com>
2020-10-29 16:38:30 -0700

    added date/time, hour variable, and conditional to set past/present/future row attributes

--- calendar_app.js
+++ calendar_app.js
@@ -1,14 +1,34 @@
+// Access HTML Elements
 var currentDayEl = document.querySelector("#currentDay");
-var timeblockContainer = document.querySelector("container");
+var timeblockContainer = document.querySelector("#container");
+var rowEl = document.querySelector(".row");
 
 
 
 
 
 // Display current day at top of calendar
-var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
 currentDayEl.textContent = currentTime;
+console.log(typeof currentTime);
 
+// Create variable for current hour
+var currentHour = parseInt(moment().format("H"));
+console.log (currentHour);
+console.log( typeof currentHour);
 
-
-// 
+// Change row appearance based on current time
+for (let i = 9; i < 18; i++) {
+    console.log([i]);   
+    if (currentHour === i) {
+        console.log("now");
+        rowEl.setAttribute("style", "color:blue; border:2px solid black;");
+        rowEl.textContent = "now!!!"[i];
+    } else if (currentHour > i) {
+        console.log("past");
+        rowEl.textContent = "past!!!"[i];
+    } else if (currentHour < i) {
+        console.log("future");
+        rowEl.textContent = "future!!!"[i];
+    }
+}
