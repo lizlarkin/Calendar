428e9e1def433180bfc1b3711cda1c5edbc761d7
Liz <lizlarkin@highlandtechnology.com>
2020-10-29 22:59:47 -0700

    added timeblock html and css code

--- calendar_app.js
+++ calendar_app.js
@@ -1,7 +1,7 @@
 // Access HTML Elements
 var currentDayEl = document.querySelector("#currentDay");
 var timeblockContainer = document.querySelector("#container");
-var rowEl = document.querySelector(".row");
+var rowEl = document.querySelectorAll(".row");
 
 
 
@@ -15,20 +15,33 @@ console.log(typeof currentTime);
 // Create variable for current hour
 var currentHour = parseInt(moment().format("H"));
 console.log (currentHour);
-console.log( typeof currentHour);
+console.log (typeof currentHour);
 
 // Change row appearance based on current time
 for (let i = 9; i < 18; i++) {
     console.log([i]);   
     if (currentHour === i) {
         console.log("now");
+        rowEl.getAttribute("id");
+        rowEl.getAttribute("input").value();
         rowEl.setAttribute("style", "color:blue; border:2px solid black;");
-        rowEl.textContent = "now!!!"[i];
+        rowEl[i].textContent = "now!!!";
     } else if (currentHour > i) {
         console.log("past");
-        rowEl.textContent = "past!!!"[i];
+        rowEl[i].textContent = "past!!!";
     } else if (currentHour < i) {
         console.log("future");
-        rowEl.textContent = "future!!!"[i];
-    }
+        rowEl[i].textContent = "future!!!";
+    } 
 }
+
+// Event when click on time block
+
+// Event when click on save button
+$("form").on("submit", function (e) {
+    e.preventDefault()
+
+})
+
+// Thomas Notes
+
