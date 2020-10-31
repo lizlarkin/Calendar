2339e1c5ef82d655cb00d46945de0b74c09ebf0a
Liz <lizlarkin@highlandtechnology.com>
2020-10-30 17:54:00 -0700

    added javascript to make scheduler dynamic

--- calendar_app.js
+++ calendar_app.js
@@ -1,47 +1,47 @@
-// Access HTML Elements
-var currentDayEl = document.querySelector("#currentDay");
-var timeblockContainer = document.querySelector("#container");
-var rowEl = document.querySelectorAll(".row");
-
-
-
-
-
+$(document).ready(function () {
 // Display current day at top of calendar
 var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
-currentDayEl.textContent = currentTime;
-console.log(typeof currentTime);
+$("#currentDay").text(currentTime);
 
-// Create variable for current hour
-var currentHour = parseInt(moment().format("H"));
-console.log (currentHour);
-console.log (typeof currentHour);
+    // Save to local storage
+    $(".saveBtn").on("click", function () {
 
-// Change row appearance based on current time
-for (let i = 9; i < 18; i++) {
-    console.log([i]);   
-    if (currentHour === i) {
-        console.log("now");
-        rowEl.getAttribute("id");
-        rowEl.getAttribute("input").value();
-        rowEl.setAttribute("style", "color:blue; border:2px solid black;");
-        rowEl[i].textContent = "now!!!";
-    } else if (currentHour > i) {
-        console.log("past");
-        rowEl[i].textContent = "past!!!";
-    } else if (currentHour < i) {
-        console.log("future");
-        rowEl[i].textContent = "future!!!";
-    } 
-}
+        var userText = $(this).siblings(".input-text").val();
+        var timeBlock = $(this).siblings(".input-text").attr("id");
+        localStorage.setItem(timeBlock, userText);
 
-// Event when click on time block
+    })
 
-// Event when click on save button
-$("form").on("submit", function (e) {
-    e.preventDefault()
+    // Change colors based on current time
+    function updateHours() {
+        var currentHour = moment().hours();
+        $(".input-text").each(function () {
+            var blockHour = parseInt($(this).attr("id"));
 
+            if (currentHour > blockHour) {
+                $(this).addClass("past");
+            } else if (currentHour === blockHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    updateHours()
+
+    // Keeps items displaying on page from local storage
+    $("#9").val(localStorage.getItem("9"));
+    $("#10").val(localStorage.getItem("10"));
+    $("#11").val(localStorage.getItem("11"));
+    $("#12").val(localStorage.getItem("12"));
+    $("#13").val(localStorage.getItem("13"));
+    $("#14").val(localStorage.getItem("14"));
+    $("#15").val(localStorage.getItem("15"));
+    $("#16").val(localStorage.getItem("16"));
+    $("#17").val(localStorage.getItem("17"));
 })
-
-// Thomas Notes
 
